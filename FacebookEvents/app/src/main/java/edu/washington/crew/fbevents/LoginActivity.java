f6b90bd4a69aed9025335bce6b771b58ebc2a95d
Nadav Ashkenazi <ashkenazi.nadav@gmail.com>
2015-05-22 00:42:09 -0700

    Set up initial login activity with Facebook SDK.

--- FacebookEvents/app/src/main/java/edu/washington/crew/fbevents/LoginActivity.java
+++ FacebookEvents/app/src/main/java/edu/washington/crew/fbevents/LoginActivity.java
@@ -1,18 +1,49 @@
 package edu.washington.crew.fbevents;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 import edu.washington.crew.fbevents.R;
 
 public class LoginActivity extends ActionBarActivity {
+    public static final String TAG = "LoginActivity";
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        callbackManager = CallbackManager.Factory.create();
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+        loginButton.setReadPermissions("user_friends", "user_events");
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.i(TAG, "Login successful");
+            }
+
+            @Override
+            public void onCancel() {
+                Log.i(TAG, "Login cancelled");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                Log.e(TAG, "Login failed");
+            }
+        });
     }
 
     @Override
--- FacebookEvents/app/src/main/java/edu/washington/crew/fbevents/MainActivity.java
+++ FacebookEvents/app/src/main/java/edu/washington/crew/fbevents/MainActivity.java
@@ -1,9 +1,11 @@
 package edu.washington.crew.fbevents;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.facebook.FacebookSdk;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +14,11 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        Intent loginIntent = new Intent(this, LoginActivity.class);
+        startActivity(loginIntent);
     }
 
     @Override
