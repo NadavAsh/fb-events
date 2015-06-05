76f9cf66547a6a773e19370a6fa4d4c64ea511e9
Nadav Ashkenazi <ashkenazi.nadav@gmail.com>
2015-06-05 16:01:16 -0700

    Add basic logout button and access token tracker to the main activity.

--- FacebookEvents/app/src/main/java/edu/washington/crew/fbevents/MainActivity.java
+++ FacebookEvents/app/src/main/java/edu/washington/crew/fbevents/MainActivity.java
@@ -3,12 +3,25 @@ package edu.washington.crew.fbevents;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 
 
 public class MainActivity extends ActionBarActivity {
+    public static final String TAG = "MainActivity";
+
+    private CallbackManager callbackManager;
+    private AccessTokenTracker accessTokenTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +29,16 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
 
-        Intent loginIntent = new Intent(this, LoginActivity.class);
-        startActivity(loginIntent);
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected  void onCurrentAccessTokenChanged(AccessToken oldToken, AccessToken newToken) {
+                updateWithToken(newToken);
+            }
+        };
+
+        updateWithToken(AccessToken.getCurrentAccessToken());
     }
 
     @Override
@@ -38,8 +58,27 @@ public class MainActivity extends ActionBarActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        } else if (id == R.id.action_logout) {
+            LoginManager.getInstance().logOut();
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void updateWithToken(AccessToken token) {
+        Log.i(TAG, "Updating with token " + token);
+        if (token == null) {
+            if (AccessToken.getCurrentAccessToken() == null) {
+                Intent loginIntent = new Intent(this, LoginActivity.class);
+                startActivity(loginIntent);
+            }
+        }
+
     }
 }
