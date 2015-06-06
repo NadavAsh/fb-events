6e5c9283ec030713ef4eb40e5cf550b7c66cd391
Nadav Ashkenazi <ashkenazi.nadav@gmail.com>
2015-06-06 13:39:40 -0700

    Fixed package build settings and access token stuff.

--- FacebookEvents/app/src/main/java/edu/washington/crew/fbevents/MainActivity.java
+++ FacebookEvents/app/src/main/java/edu/washington/crew/fbevents/MainActivity.java
@@ -1,8 +1,13 @@
 package edu.washington.crew.fbevents;
 
 import android.content.Intent;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,15 +15,22 @@ import android.view.MenuItem;
 import com.facebook.AccessToken;
 import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginManager;
+
+import org.json.JSONObject;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends ActionBarActivity {
     public static final String TAG = "MainActivity";
 
     private CallbackManager callbackManager;
     private AccessTokenTracker accessTokenTracker;
+    private AccessToken accessToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +42,27 @@ public class MainActivity extends ActionBarActivity {
 
         accessTokenTracker = new AccessTokenTracker() {
             @Override
-            protected  void onCurrentAccessTokenChanged(AccessToken oldToken, AccessToken newToken) {
+            protected void onCurrentAccessTokenChanged(AccessToken oldToken, AccessToken newToken) {
                 updateWithToken(newToken);
             }
         };
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
 
-        updateWithToken(AccessToken.getCurrentAccessToken());
+        GraphRequest request = GraphRequest.newMeRequest(accessToken, new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject jsonObject, GraphResponse graphResponse) {
+                if (jsonObject != null) {
+                    Log.i(TAG, jsonObject.toString());
+                }
+            }
+        });
+        Bundle params = new Bundle();
+        params.putString("fields", "id,name,link");
+        request.setParameters(params);
+        request.executeAsync();
     }
 
     @Override
@@ -77,12 +104,10 @@ public class MainActivity extends ActionBarActivity {
 
     private void updateWithToken(AccessToken token) {
         Log.i(TAG, "Updating with token " + token);
-        if (token == null) {
-            if (AccessToken.getCurrentAccessToken() == null) {
-                Intent loginIntent = new Intent(this, LoginActivity.class);
-                startActivity(loginIntent);
-            }
+        if (token == null || token.isExpired()) {
+            Intent loginIntent = new Intent(this, LoginActivity.class);
+            startActivity(loginIntent);
         }
-
+        accessToken = token;
     }
 }
